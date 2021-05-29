9d82a99552b290f574c35b52c93bf7a3a6187e76
박재민 <pparkjae0405@naver.com>
2021-05-29 22:13:58 +0900

    Update

---
+++ 7. 문자열/1. 11654.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = br.readLine().charAt(0);
+        System.out.println(a);
+    }
+}
