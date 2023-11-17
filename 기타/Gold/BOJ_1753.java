e847689357679e5cad4a7f60cf35d1854deeaf90
박재민 <pparkjae0405@naver.com>
2023-11-17 19:31:10 +0900

    백준 문제풀이
    - 1197번, 1753번, 1922번, 1012번, 11403번

--- 기타/Gold/BOJ_1753.java
+++ 기타/Gold/BOJ_1753.java
@@ -3,60 +3,52 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
-    // 1. 노드 정보 리스트 graph, 노드 방문 여부 visited, 최단 거리 d 선언
-
+public class Main {
+    // 1. 정점의 개수 V, 간선의 개수 E,
+    static int V, E;
+    // 연결 정보 graph, 정점 방문 여부 visited,
     static ArrayList<Node>[] graph;
     static boolean[] visited;
+    // 최단 경로 d를 선언한다.
     static int[] d;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        /*
-        다익스트라 알고리즘
-        특정한 하나의 정점에서 모든 정점으로의 최단경로를 구하는 알고리즘
-        1. 가중치 기준 오름차순으로 우선순위 큐를 선언한다.
-        2. 출발 노드를 우선순위 큐에 추가하고 d[출발 노드]를 0으로 초기화한다.
-        3. pq의 제일 위에 있는 노드를 빼내 방문하지 않았을 경우 방문처리하고
-        4. 해당 노드와 연결된 Node를 가져와 방문하지 않았고 최단거리 > 현재 노드를 경유하여 가는 경우일 경우 최단거리를 갱신하고 pq에 추가한다.
-        5. 3~4를 pq가 빌 때 까지 반복한다.
-        */
-
-        // 2. 정점의 개수 V와 간선의 개수 E, 시작 정점의 번호 K가 주어진다.
+        // 2. V, E가 주어지고
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
-
-        // 3. graph, visited, d의 크기를 설정하고
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        // graph, visited, d의 크기를 설정한 뒤
         graph = new ArrayList[V + 1];
         visited = new boolean[V + 1];
         d = new int[V + 1];
-
-        // graph를 초기화하고 d를 INF으로 저장한다.
+        // graph를 초기화하면서 d를 INF로 저장한다.
         for(int i = 1 ; i <= V ; i++){
             graph[i] = new ArrayList<>();
             d[i] = Integer.MAX_VALUE;
         }
 
-        // 4. E개의 시작노드 u, 도착 노드 v, 가중치 w가 주어지고,
+        // 3. 시작 정점 K가 주어지고
+        int K = Integer.parseInt(br.readLine());
+        // E만큼의 방향그래프 정보를 graph에 저장한다.
         for(int i = 0 ; i < E ; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            // graph에 저장한다.
-            graph[u].add(new Node(v, w));
+            graph[start].add(new Node(end, cost));
         }
 
-        // 5. 다익스트라 알고리즘을 수행한 결과를 출력한다.
+        // 4. 다익스트라 알고리즘을 호출한 결과를 출력한다.
         dijkstra(K);
         for(int i = 1 ; i <= V ; i++){
-            if(d[i] == Integer.MAX_VALUE) bw.write("INF" + "\n");
-            else bw.write(d[i] + "\n");
+            if(d[i] == Integer.MAX_VALUE){
+                bw.write("INF" + "\n");
+            }else{
+                bw.write(d[i] + "\n");
+            }
         }
 
         bw.flush();
@@ -65,7 +57,6 @@ public class Main{
     }
 
     static class Node{
-        // (연결된 간선 v, 간선까지의 거리 cost)
         int v;
         int cost;
 
@@ -75,26 +66,26 @@ public class Main{
         }
     }
 
-    static void dijkstra(int start) {
-        // 가중치 기준 오름차순으로 우선순위 큐를 선언하고
+    static void dijkstra(int start){
+        // 가중치 기준 오름차순으로 우선순위 큐를 선언한다.
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
 
-        // (시작 노드, 0)을 추가한 뒤 d[start]를 0으로 바꿔준 뒤
-        pq.add(new Node(start, 0));
+        // 시작 위치를 0으로 설정하고 pq에 추가한다.
         d[start] = 0;
+        pq.add(new Node(start, 0));
 
-        // pq가 빌 때까지 반복하여
-        while (!pq.isEmpty()) {
-            // pq의 제일 위에 있는 노드를 빼내 방문하지 않았을 경우 방문처리하고
+        // pq가 빌 때 까지
+        while(!pq.isEmpty()){
+            // 현재 정보를 가져와
             Node now = pq.poll();
-            if (!visited[now.v]) {
-                visited[now.v] = true;
-            }
+            // 방문하지 않았을 경우 방문처리하고
+            if(!visited[now.v]) visited[now.v] = true;
 
-            // 현재 노드와 연결된 (v, cost)를 next로 가져와
-            for (Node next : graph[now.v]) {
-                // next가 방문하지 않았고, 최단거리가 현재 노드를 경유하여 가는 경우가 더 짧을 경우
-                if (!visited[next.v] && d[next.v] > now.cost + next.cost) {
+            // 연결 정보를 가져와
+            for(Node next : graph[now.v]){
+                // 방문하지 않았고
+                // 다음 최단거리 > 현재 가중치 + 다음 가중치일 경우
+                if(!visited[next.v] && d[next.v] > now.cost + next.cost){
                     // 최단거리를 갱신하고 pq에 추가한다.
                     d[next.v] = now.cost + next.cost;
                     pq.add(new Node(next.v, d[next.v]));
--- 기타/Silver/BOJ_1012.java
+++ 기타/Silver/BOJ_1012.java
@@ -1,82 +1,94 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-    // 1. 배추밭의 가로길이 M, 세로길이 N, 배추 위치 크기 K,
+public class Main {
+    // 1. 배추밭의 가로길이 M, 세로길이 N, 배추의 개수 K,
     static int M, N, K;
-    // 배추밭 field, 방문여부 visited, 이동방향 dx, dy를 선언한다.
-    static int[][] field;
+    // 배추밭 정보 info, 방문 여부 visited,
+    static int[][] info;
     static boolean[][] visited;
+    // 이동 방향 dx, dy, bfs를 위한 큐,
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-
+    static Queue<int[]> queue = new LinkedList<>();
+    // 필요한 배추흰지렁이의 개수 count를 선언한다.
+    static int count;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 2. 테스트 케이스 개수 T가 주어진다.
-        int T = Integer.parseInt(br.readLine());
-
-        // 3. 테스트 케이스 개수만큼 반복하여
+        // 2. 테스트 케이스 개수 T가 주어지고
         StringTokenizer st;
-        for(int i = 0 ; i < T ; i++){
-            // M, N, K를 입력받고
+        int T = Integer.parseInt(br.readLine());
+        for(int a = 0 ; a < T ; a++){
+            // M, N, K가 주어지면
             st = new StringTokenizer(br.readLine(), " ");
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
-            // field와 visited의 크기를 N*M만큼 설정한 뒤
-            field = new int[N][M];
+            // info, visited, count의 크기를 설정하고
+            info = new int[N][M];
             visited = new boolean[N][M];
+            count = 0;
 
-            // 4. K의 개수만큼 배추 위치를 입력받아
-            for(int j = 0 ; j < K ; j++){
+            // info 정보를 저장한 뒤
+            for(int i = 0 ; i < K ; i++){
                 st = new StringTokenizer(br.readLine(), " ");
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
 
-                // 심어진 배추를 설정한다.
-                field[y][x] = 1;
+                info[y][x] = 1;
             }
 
-            // 5. 배추밭을 돌면서
-            int count = 0;
-            for(int j = 0 ; j < N ; j++){
-                for(int k = 0 ; k < M ; k++){
-                    // 방문하지 않은 배추를 찾고
-                    if(field[j][k] == 1 && !visited[j][k]){
-                        // 배추밭 내의 연결된 배추 그룹의 수를 그래프 탐색을 통해 찾아
-                        dfs(j, k);
+            // info를 돌면서
+            for(int i = 0 ; i < N ; i++){
+                for(int j = 0 ; j < M ; j++){
+                    // 방문하지 않은 배추가 있다면
+                    if(!visited[i][j] && info[i][j] == 1){
+                        // bfs를 호출하여 count를 센다.
+                        bfs(i, j);
                         count++;
                     }
                 }
             }
 
-            // 결과를 출력한다.
+            // 3. count를 출력한다.
             bw.write(count + "\n");
         }
 
         bw.flush();
-        br.close();
         bw.close();
+        br.close();
     }
 
-    static void dfs(int x, int y) {
-        // 찾은 위치를 방문처리하고
-        visited[x][y] = true;
+    static void bfs(int y, int x){
+        // 시작 위치를 방문처리하고 큐에 추가한다.
+        visited[y][x] = true;
+        queue.add(new int[]{y, x});
 
-        // 인접 위치를 탐색하여
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
+        // 큐가 빌 때 까지
+        while(!queue.isEmpty()){
+            // 현재 위치를 가져와
+            int[] now = queue.poll();
+            int nowY = now[0];
+            int nowX = now[1];
 
-            // (nextX, nextY) 좌표가 밭 범위 안에 존재하고
-            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
-                // 해당 위치가 1이고 방문하지 않았을경우
-                if (field[nextX][nextY] == 1 && !visited[nextX][nextY]) {
-                    // 해당 위치로 이동시킨다.
-                    dfs(nextX, nextY);
+            // 인접 위치를 탐색하는데
+            for(int i = 0 ; i < 4 ; i++){
+                int nextY = nowY + dy[i];
+                int nextX = nowX + dx[i];
+
+                // 해당 위치가 배추밭 범위 내고
+                if(nextY >= 0 && nextY < N && nextX >= 0 && nextX < M){
+                    // 방문하지 않은 배추가 있다면
+                    if(!visited[nextY][nextX] && info[nextY][nextX] == 1){
+                        // 해당 위치를 방문처리하고 큐에 추가한다.
+                        visited[nextY][nextX] = true;
+                        queue.add(new int[]{nextY, nextX});
+                    }
                 }
             }
         }
